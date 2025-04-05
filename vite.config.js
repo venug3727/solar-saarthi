be6446aa16dfbdf8730ca0f77836e81c4166ee68
Venugopal C S <venug3727@gmail.com>
2025-04-05 21:00:55 +0530

    netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      "Content-Type": "application/javascript",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
     },
   },
 });
